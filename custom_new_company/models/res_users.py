e7507ee80902da3229e53e3b851f6837d0f800ac
aymafaza <yusqie@alterra.id>
2022-08-02 10:43:16 +0700

    fix error custom  module

--- custom_new_company/models/res_users.py
+++ custom_new_company/models/res_users.py
@@ -10,7 +10,7 @@ class ResUsers(models.Model):
     def _signup_create_user(self, values):
         # Create new company on sign up with defined oauth provider
         provider = self.env['ir.config_parameter'].sudo().get_param('auth_signup.company_on_provider.provider_id')
-        if values['oauth_uid'] and values['oauth_provider_id'] == provider.id:
+        if values['oauth_uid'] and values['oauth_provider_id'] == provider:
             company = self.env['res.company'].create({'name': values['name']})
             values['company_id'] = company.id
             values['company_ids'] = [(4, company.id)]
